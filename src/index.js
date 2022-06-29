0170b01e94f135855d10ba1116368af0281451e6
myociss <yocissms@gmail.com>
2022-06-28 21:39:22 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,174 +1,237 @@
 import * as THREE from 'three'
 import { WEBGL } from './webgl'
-import './modal'
+//import './modal'
+import { GPUComputationRenderer } from 'three/examples/jsm/misc/GPUComputationRenderer'
 
 if (WEBGL.isWebGLAvailable()) {
-  var camera, scene, renderer
-  var plane
-  var mouse,
-    raycaster,
-    isShiftDown = false
+  var camera, scene, renderer, gpuCompute, geometry, positionVariable, velocityVariable, accelerationVariable,
+    positionUniforms, velocityUniforms, points,
+    accelerationTextureIndex, velocityTextureIndex, positionTextureIndex;
 
-  var rollOverMesh, rollOverMaterial
-  var cubeGeo, cubeMaterial
+  var textureDim = 8;
+  var nParticles = textureDim * textureDim;
 
-  var objects = []
+  //var particleScale = 400;
+  var particleStartX = -1200.0;
+  var particleYConst = 80.0;
 
-  init()
-  render()
+  var deltaT = 0.39269908;
 
-  function init() {
+  initScene();
+  initGPU();
+  initParticles();
+
+  //points.material.uniforms.texturePosition.value = gpuCompute.getAlternateRenderTarget( positionVariable ).texture;
+
+  /*computeAcceleration();
+
+  computeVelocity();
+  computePosition();
+  computeAcceleration();
+
+  points.material.uniforms.texturePosition.value = positionVariable.renderTargets[positionTextureIndex].texture;
+  //console.log(points.material.uniforms.texturePosition.value.image);
+
+  render();*/
+
+  computeAcceleration();
+
+  //animate();
+
+  //render();
+  //render();
+
+  //for (var a=0; a < 2; a++){
+  //  render();
+  //}
+
+  function initScene() {
     camera = new THREE.PerspectiveCamera(
       45,
       window.innerWidth / window.innerHeight,
-      1,
-      10000
-    )
-    camera.position.set(500, 800, 1300)
-    camera.lookAt(0, 0, 0)
+      5,
+      3500
+    );
 
-    scene = new THREE.Scene()
-    scene.background = new THREE.Color(0xf0f0f0)
+    camera.position.z = 2750;
 
-    var rollOverGeo = new THREE.BoxBufferGeometry(50, 50, 50)
-    rollOverMaterial = new THREE.MeshBasicMaterial({
-      color: 0xff0000,
-      opacity: 0.5,
-      transparent: true,
-    })
-    rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial)
-    scene.add(rollOverMesh)
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x050505);
+    scene.fog = new THREE.Fog( 0x050505, 2000, 3500 );
 
-    cubeGeo = new THREE.BoxBufferGeometry(50, 50, 50)
-    cubeMaterial = new THREE.MeshLambertMaterial({
-      color: 0xfeb74c,
-      map: new THREE.TextureLoader().load('static/textures/square.png'),
-    })
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-    var gridHelper = new THREE.GridHelper(1000, 20)
-    scene.add(gridHelper)
+    window.addEventListener('resize', onWindowResize, false);
 
-    raycaster = new THREE.Raycaster()
-    mouse = new THREE.Vector2()
+  }
 
-    var geometry = new THREE.PlaneBufferGeometry(1000, 1000)
-    geometry.rotateX(-Math.PI / 2)
+  function initGPU(){
+    accelerationTextureIndex = 0;
+    velocityTextureIndex = 0;
+    positionTextureIndex = 0;
 
-    plane = new THREE.Mesh(
-      geometry,
-      new THREE.MeshBasicMaterial({ visible: false })
-    )
-    scene.add(plane)
+    gpuCompute = new GPUComputationRenderer(textureDim,textureDim,renderer);
 
-    objects.push(plane)
+    var dtPosition = gpuCompute.createTexture();
 
-    var ambientLight = new THREE.AmbientLight(0x606060)
-    scene.add(ambientLight)
+    var posArray = dtPosition.image.data;
 
-    var directionalLight = new THREE.DirectionalLight(0xffffff)
-    directionalLight.position.set(1, 0.75, 0.5).normalize()
-    scene.add(directionalLight)
+    for (var i=0; i < posArray.length; i+=4){
+      var x = particleStartX;
+      var y = particleStartX + particleYConst * (i / 4);
+      var z = 0;
 
-    renderer = new THREE.WebGLRenderer({ antialias: true })
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
+      posArray[i + 0] = x;
+      posArray[i + 1] = y;
+      posArray[i + 2] = z;
+      posArray[i + 3] = 1;
+    }
 
-    document.addEventListener('mousemove', onDocumentMouseMove, false)
-    document.addEventListener('mousedown', onDocumentMouseDown, false)
-    document.addEventListener('keydown', onDocumentKeyDown, false)
-    document.addEventListener('keyup', onDocumentKeyUp, false)
-    window.addEventListener('resize', onWindowResize, false)
+	  var dtVelocity = gpuCompute.createTexture();
+
+    var velArray = dtVelocity.image.data;
+
+    for (var i=0; i < velArray.length; i+=4){
+      velArray[i + 0] = deltaT;
+      velArray[i + 1] = 0;
+      velArray[i + 2] = 0;
+      velArray[i + 3] = 0;
+    }
+
+    var dtAcceleration = gpuCompute.createTexture();
+
+    var accArray = dtAcceleration.image.data;
+
+    for (var i=0; i < accArray.length; i+=4){
+      accArray[i + 0] = 0;
+      accArray[i + 1] = 0;
+      accArray[i + 2] = 0;
+      accArray[i + 3] = 0;
+    }
+
+    positionVariable = gpuCompute.addVariable("texturePosition",
+        document.getElementById('fragmentShaderPosition').textContent, dtPosition);
+
+    velocityVariable = gpuCompute.addVariable("textureVelocity",
+        document.getElementById('fragmentShaderVelocity').textContent, dtVelocity);
+
+    accelerationVariable = gpuCompute.addVariable("textureAcceleration",
+        document.getElementById('fragmentShaderAcceleration').textContent, dtAcceleration);
+
+    gpuCompute.setVariableDependencies(accelerationVariable, [positionVariable]);
+    gpuCompute.setVariableDependencies(velocityVariable, [velocityVariable, accelerationVariable]);
+    gpuCompute.setVariableDependencies(positionVariable, [positionVariable, velocityVariable]);
+
+    positionUniforms = positionVariable.material.uniforms;
+    positionUniforms.deltaT = {value: deltaT};
+
+    velocityUniforms = velocityVariable.material.uniforms;
+    velocityUniforms.deltaT = {value: deltaT};
+
+    var error = gpuCompute.init();
+    if ( error !== null ) {
+      console.error( error );
+    }
+  }
+
+
+  function initParticles(){
+
+    geometry = new THREE.BufferGeometry();
+
+    var vertices = new THREE.BufferAttribute( new Float32Array( nParticles * 3 ), 3 );
+    var references = new THREE.BufferAttribute( new Float32Array( nParticles * 2 ), 2 );
+
+    for (var i=0; i<nParticles; i++){
+        var u = (i % textureDim) / textureDim;
+        var v = ~~(i/textureDim) / textureDim;
+        references.array[i * 2] = u;
+        references.array[i * 2 + 1]=v;
+    }
+
+    geometry.setAttribute('position',vertices);
+    geometry.setAttribute('reference',references);
+
+
+    var material = new THREE.ShaderMaterial({
+      uniforms: THREE.UniformsUtils.merge( [
+        THREE.UniformsLib[ 'fog' ], { texturePosition: {value: null} }
+      ] ),
+      vertexShader: document.getElementById('physicsVertexShader').textContent,
+      fragmentShader: document.getElementById('pointFragmentShader').textContent,
+      vertexColors: true,
+      fog: true
+    });
+
+    //material.needsUpdate = true;
+
+    points = new THREE.Points( geometry, material );
+    //points.matrixAutoUpdate = false;
+    //points.updateMatrix();
+    scene.add( points );
   }
 
   function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
-  function onDocumentMouseMove(event) {
-    event.preventDefault()
-
-    mouse.set(
-      (event.clientX / window.innerWidth) * 2 - 1,
-      -(event.clientY / window.innerHeight) * 2 + 1
-    )
-
-    raycaster.setFromCamera(mouse, camera)
-
-    var intersects = raycaster.intersectObjects(objects)
-
-    if (intersects.length > 0) {
-      var intersect = intersects[0]
-
-      rollOverMesh.position.copy(intersect.point).add(intersect.face.normal)
-      rollOverMesh.position
-        .divideScalar(50)
-        .floor()
-        .multiplyScalar(50)
-        .addScalar(25)
-    }
-
-    render()
+  function computeAcceleration(){
+    var uniforms = accelerationVariable.material.uniforms;
+    var nextIdx = accelerationTextureIndex === 0 ? 1 : 0;
+    uniforms['texturePosition'].value = positionVariable.renderTargets[ positionTextureIndex ].texture;
+    var target = accelerationVariable.renderTargets[nextIdx];
+    gpuCompute.doRenderTarget(accelerationVariable.material,target);
+    accelerationTextureIndex = nextIdx;
+    //alert('hm...');
   }
 
-  function onDocumentMouseDown(event) {
-    event.preventDefault()
+  function computeVelocity(){
+    var uniforms = velocityVariable.material.uniforms;
+    var nextIdx = velocityTextureIndex === 0 ? 1 : 0;
+    uniforms['textureAcceleration'].value = accelerationVariable.renderTargets[ accelerationTextureIndex ].texture;
+    uniforms['textureVelocity'].value = velocityVariable.renderTargets[ velocityTextureIndex ].texture;
 
-    mouse.set(
-      (event.clientX / window.innerWidth) * 2 - 1,
-      -(event.clientY / window.innerHeight) * 2 + 1
-    )
-
-    raycaster.setFromCamera(mouse, camera)
-
-    var intersects = raycaster.intersectObjects(objects)
-
-    if (intersects.length > 0) {
-      var intersect = intersects[0]
-
-      if (isShiftDown) {
-        if (intersect.object !== plane) {
-          scene.remove(intersect.object)
-
-          objects.splice(objects.indexOf(intersect.object), 1)
-        }
-
-      } else {
-        var voxel = new THREE.Mesh(cubeGeo, cubeMaterial)
-        voxel.position.copy(intersect.point).add(intersect.face.normal)
-        voxel.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25)
-        scene.add(voxel)
-
-        objects.push(voxel)
-      }
-
-      render()
-    }
+    var target = velocityVariable.renderTargets[nextIdx];
+    gpuCompute.doRenderTarget(velocityVariable.material,target);
+    velocityTextureIndex = nextIdx;
   }
 
-  function onDocumentKeyDown(event) {
-    switch (event.keyCode) {
-      case 16:
-        isShiftDown = true
-        break
-    }
+  function computePosition(){
+    var uniforms = positionVariable.material.uniforms;
+    var nextIdx = positionTextureIndex === 0 ? 1 : 0;
+    uniforms['texturePosition'].value = positionVariable.renderTargets[ positionTextureIndex ].texture;
+    uniforms['textureVelocity'].value = velocityVariable.renderTargets[ velocityTextureIndex ].texture;
+
+    var target = positionVariable.renderTargets[nextIdx];
+    gpuCompute.doRenderTarget(positionVariable.material,target);
+    positionTextureIndex = nextIdx;
   }
 
-  function onDocumentKeyUp(event) {
-    switch (event.keyCode) {
-      case 16:
-        isShiftDown = false
-        break
-    }
+  function animate(){
+    requestAnimationFrame(animate);
+    render();
   }
 
   function render() {
-    renderer.render(scene, camera)
+    //computeAcceleration();
+
+    computeVelocity();
+    computePosition();
+    computeAcceleration();
+    computeVelocity();
+
+    points.material.uniforms.texturePosition.value = positionVariable.renderTargets[positionTextureIndex].texture;
+
+    renderer.render(scene, camera);
   }
+
 } else {
-  var warning = WEBGL.getWebGLErrorMessage()
-  document.body.appendChild(warning)
+  var warning = WEBGL.getWebGLErrorMessage();
+  document.body.appendChild(warning);
 }
